f6e18a7b7a9beb09e6e4ccb05a33b63ca9326194
MrTaciturne <Borys_3000@mail.ru>
2023-09-25 22:19:34 +0300

    СПРИНГА

--- src/main/java/com/example/springstarter/controller/UserController.java
+++ src/main/java/com/example/springstarter/controller/UserController.java
@@ -6,7 +6,6 @@ import com.example.springstarter.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.management.MemoryType;
 import java.util.List;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
